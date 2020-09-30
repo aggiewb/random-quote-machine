80402adeac2a8d1e2a953dc81d10a36e79033753
AggieWB <usmaggie23@gmail.com>
2020-09-30 14:50:15 -0700

    Change App function to class which extends React.Component
    Add constructor with state quote and author properties
    Add methods componentDidMount(), getQuote() which fetches a quote from a Simpons quote api, and render()
    Add QuoteBox component, which returns elements required in User Stories #1-7

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,44 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return <div className="App">
-  </div>;
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      quote: '',
+      author: ''
+    }
+  }
+
+  componentDidMount(){
+    this.getQuote();
+  }
+
+  getQuote(){
+    fetch('https://thesimpsonsquoteapi.glitch.me/quotes', {
+      method: 'GET'
+    })
+    .then(response => response.json())
+    .then(data => {
+      this.setState({quote: data[0].quote});
+      this.setState({author: data[0].character});
+    })
+  }
+  
+  render(){
+    return <main className="App">
+      <QuoteBox quote={this.state.quote} author={this.state.author}/>
+    </main>;
+  }
+}
+
+function QuoteBox(props){
+  return <section id="quote-box">
+    <p id="text">{props.quote}</p>
+    <p id="author">{props.author}</p>
+    <button id="new-quote">New Quote</button>
+    <button id="tweet-quote">Tweet Quote</button>
+  </section>;
 }
 
 export default App;
