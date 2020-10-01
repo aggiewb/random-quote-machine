4052d146dadc0b7ea0a0a35afdf0f867da0b5ecf
AggieWB <usmaggie23@gmail.com>
2020-10-01 12:56:33 -0700

    Remove testing-library/react module, and import shallow and
    mount methods from the enzyme module
    Add 'smoke test' for App rendering

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,7 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { shallow, mount } from 'enzyme';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+it('renders', () => {
+  mount(<App />);
 });
