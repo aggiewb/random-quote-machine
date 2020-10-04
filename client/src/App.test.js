10cd7594240f7feb167a9c364c27e73dcd8c8432
AggieWB <usmaggie23@gmail.com>
2020-10-04 15:39:29 -0700

    Update EXPECTED_ERROR value, add test for when an update
    occurs to the QuoteBox props with data from the quote API after mounting, and add test when an assignment of a String value to the QuoteBox error prop occurs

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -8,7 +8,7 @@ import App, { QuoteBox, UserActions, Footer } from './App';
 
 const EXPECTED_QUOTE = 'Boop';
 const EXPECTED_AUTHOR = 'Steven B';
-const EXPECTED_ERROR = 'Help';
+const EXPECTED_ERROR = 'An error has occured. Please try again later.';
 
 it('renders', () => {
   mount(<App />);
@@ -26,4 +26,44 @@ it('should shallow render App class child components, and initialize their props
 
   const footer = app.find('Footer');
   expect(footer.exists()).toEqual(true);
+});
+
+it('should update the QuoteBox props with data from the quote API after mounting', (done) => {
+  global.fetch = () => {
+    return new Promise(resolve => {
+      resolve({
+        json: () => new Promise(resolve => {
+          resolve([{quote: EXPECTED_QUOTE, character: EXPECTED_AUTHOR}]);
+        })
+      });
+    });
+  };
+
+  const app = shallow(<App />);
+
+  process.nextTick(() => {
+    const quoteBox = app.find('QuoteBox');
+    expect(quoteBox.prop('quote')).toEqual(EXPECTED_QUOTE);
+    expect(quoteBox.prop('author')).toEqual(EXPECTED_AUTHOR);
+    expect(quoteBox.prop('error')).toEqual('');
+    done();
+  });
+});
+
+it('should assign a String value to the QuoteBox error prop', (done) => {
+  global.fetch = () => {
+    return new Promise((resolve, reject) => {
+      reject();
+    });
+  };
+
+  const app = shallow(<App />);
+
+  process.nextTick(() => {
+    const quoteBox = app.find('QuoteBox');
+    expect(quoteBox.prop('quote')).toEqual('');
+    expect(quoteBox.prop('author')).toEqual('');
+    expect(quoteBox.prop('error')).toEqual(EXPECTED_ERROR);
+    done();
+  });
 });
