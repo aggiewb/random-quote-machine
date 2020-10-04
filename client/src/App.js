1a733f8feb4964a5d8e6d161ae74f8909aaea393
AggieWB <usmaggie23@gmail.com>
2020-10-04 14:47:38 -0700

    Export functions for testing

--- client/src/App.js
+++ client/src/App.js
@@ -36,7 +36,7 @@ class App extends React.Component{
   }
 }
 
-function QuoteBox(props){
+export function QuoteBox(props){
   return <section id="quote-box">
   <p id="text">{props.error ? props.error : props.quote}</p>
   <p id="author">{props.author}</p>
@@ -44,14 +44,14 @@ function QuoteBox(props){
 </section>;
 }
 
-function UserActions(props){
+export function UserActions(props){
   return <section id="button-box">
       <button id="new-quote" onClick={props.handleNewQuote}>New Quote</button>
       <a href={`https://twitter.com/intent/tweet?text=${props.quote} -${props.author}`} target="blank" id="tweet-quote">Tweet Quote</a>
   </section>;
 }
 
-function Footer(){
+export function Footer(){
   return <footer>
     <div>
       <a href="https://www.aggiewb.com" rel="noreferrer noopener" target="_blank">Aggie Wheeler Bateman</a> &copy; {new Date().getFullYear()}
