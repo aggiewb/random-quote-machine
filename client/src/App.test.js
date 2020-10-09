afafb9d80eef62dbac44495d4c4786d366859ea4
AggieWB <usmaggie23@gmail.com>
2020-10-09 09:18:25 -0700

    Add test for rendering QuoteBox p tag elements containing
    error prop when it is truthy

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -77,7 +77,15 @@ it('should render QuoteBox p tag elements with ids of text and author containing
   expect(author.text()).toEqual(EXPECTED_AUTHOR);
 });
 
-//TODO: should render QuoteBox p tag elements containing error prop when it is truthy
+it('should render QuoteBox p tag elements containing error prop when it is truthy', () => {
+  const quoteBox = shallow(<QuoteBox quote={''} author={''} error={EXPECTED_ERROR} />);
+  const text = quoteBox.find('#text');
+  const author = quoteBox.find('#author');
+
+  expect(text.text()).toEqual(EXPECTED_ERROR);
+  expect(author.text()).toEqual('');
+  expect(author.text()).toBeFalsy();
+})
 //TODO: should render UserActions and pass props
 //TODO: should render expect UserAction elements on load
 //TODO: should be able to click button within UserAction
